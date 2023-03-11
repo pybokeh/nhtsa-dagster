d2a70768e8e9d74775d50cf2f3948ce668cc0da2
pybpkeh <pybokeh@protonmail.com>
2023-03-11 12:26:16 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,127 @@
+## NHTSA vPIC API Extract using python and dagster orchestration framework
+
+
+### BACKGROUND
+NHTSA (National Highway Transportation and Safety Administration) is a U.S. government agency responsible for tracking
+vehicle safety.  As a consequence of this responsibility, vehicle manufacturers are required by law to provide vehicle
+information to NHTSA.  This information includes basic VIN decode information.  In other words, for a given VIN or VIN
+pattern, vehicle manufacturers must provide at minimum some basic vehicle information, such as model year, make, model,
+manufacturer, etc.
+
+NHTSA provides a free API service called "vPIC".  Information about its vPIC api can be accessed [here](https://vpic.nhtsa.dot.gov/api/).
+
+With data extracted from NHTSA's vPIC api, we can create useful reference tables containing various vehicle information.
+
+For example, we can obtain information about all the applicable model names under a given vehicle make.  We can also
+find out which model years did a certain model was in existence.
+
+### PURPOSE
+The purpose of this code repository is to be able to extract information from NHTSA's vPIC api using python scripting.
+The implementation contained in this repository saves the data as Snowflake tables.  However, we can choose to use a
+different database if desired.
+<br><br><br>
+
+#### Why use an orchestration framework
+The implementation contained in this repository uses a Python orchestration library called [dagster](https://docs.dagster.io/).  Using an
+orchestration library is recommended especially if your tasks need to be repeated or scheduled.  There are other
+reasons for using various orchestration libraries as they each have their own set of features.  One nice feature that
+comes with dagster is it shows you what is known as data lineage - a way for you to visually see the dependencies of
+your data assets.  It also shows you when each data asset was last materialized or created.  Dagster also comes with
+various [integrations](https://docs.dagster.io/integrations) or plug-ins for working with Snowflake, Big Query, PySpark, pandas, dbt, Fivetran, etc. Another nice feature with dagster, it is very "local development friendly".  What this means is,
+to use dagster, you aren't required or forced to develop using web-based or cloud-based tools or have to connect to a 
+remote computer.  You can simply develop using your local Python development installed on your machine.  Dagster is even 
+Windows OS friendly, unlike other orchestration frameworks that require you to use Linux/Mac OS or WSL2 or complex docker setup.
+
+
+#### Getting started with dagster
+Before getting started with dagster, it is highly recommended you view this short 26-minute YouTube [video](https://youtu.be/sCUYBa-glh0).  It should
+give you a good sense of what dagster does and why such library can be useful.  Anyone with intermediate level of Python
+experience should be able to follow the example.
+
+- [Blog](https://dagster.io/blog/dagster-script-to-assets)
+- [Code example](https://github.com/petehunt/dagster-script-to-assets)
+- Official dagster [documentation](https://docs.dagster.io/getting-started) where you can find their [tutorial](https://docs.dagster.io/tutorial).
+
+
+#### Installing a Python virtual environment (Windows 10)
+It is highly recommended that you install what is known as a Python virtual environment using the built-in "venv" module.
+After you have installed Python, you can issue the following command to create a virtual environment called "dagster_dev"
+or whatever you want to call yours:
+`python -m venv dagster_dev`
+
+
+Then change directory into the newly created `dagster_dev` folder: `cd dagster_dev`
+
+
+Then when you're within the `dagster_dev` folder, you can then activate it, by issuing the following command if using
+Windows terminal:
+`Scripts\activate.bat`
+
+Then you can install dagster related packages using the `pip` command:
+`pip install dagster dagit`
+
+In this code repo, the actual packages that I installed are the following:
+
+`pip install dagster dagit dagster-snowflake dagster-snowflake-pandas requests tqdm pylint black pytest loguru`
+
+I then created my `requirements.txt` file by issuing the following command:
+
+`pip freeze > requirements.txt`
+
+This special file contains all the Python packages that I've installed in this code repository.  If you wanted to duplicate
+my Python environment, you can simply import this `requirements.txt` file (NOTE: this particular environment would only be valid if you're on Windows 10)
+
+
+#### Configuring Dagster for non-PROD with default sqlite backend
+Before we can use dagster, we need to first create a `DAGSTER_HOME` environment variable that is set to the path 
+containing your `dagster.yaml` file.
+
+
+For instance, if your `dagster.yaml` file is located at: `C:\Users\your_user\.dagster\dagster.yaml`, then you can set
+or create a new `DAGSTER_HOME` environment variable via `set` command:
+`set DAGSTER_HOME=C:\Users\your_user\.dagster`
+
+
+or the `export` command if you're not on Windows OS:
+`export DAGSTER_HOME=/home/your_user/.dagster`
+
+
+This special file is where you configure your dagster instance.  For information on all the things you can configure with
+your local dagster instance, view this [page](https://docs.dagster.io/deployment/dagster-instance).  For just starting
+off with local sqlite backend, your `dagster.yaml` file can just contain the following lines:
+```
+telemetry:
+  enabled: false
+```
+
+
+What does this sqlite backend do?  It just stores runtime information when dagster executes your tasks or pipelines.
+Information such as when the runs started, when they failed, how long they took to finish, etc.  You can also choose to
+use a more robust database backend, such as PostgreSQL.
+
+
+#### Storing "secrets" / password credentials
+Per dagster's [documentation](https://docs.dagster.io/guides/dagster/using-environment-variables-and-secrets#using-environment-variables-and-secrets), they recommend that we save sensitive type of information in a simple text file called
+`.env` that you of course, do NOT check into code repository.  Make sure to add this in your `.gitignore` file.
+What dagster then does is, it saves your credentials as environment variables that you can refer to in your dagster code.  
+In this code repo, Snowflake was used to store NHTSA's data.  Therefore, credentials for Snowflake were saved in this `.env` file.
+
+
+#### Running dagster and it's web UI called dagit
+Assuming you have Python installed and your virtual environment activated, you can execute or run dagster's `dagit` web 
+UI by changing directory into the `src` directory and then issuing the following command:
+
+
+`dagster dev -f nhtsa_assets.py`
+
+
+#### What your dagster assets should look like
+When using the code in this repo and have dagster's `dagit` web UI up and running, you should have an asset lineage
+graph that looks similar to below:
+
+
+![alt text](images/asset_lineage.png)
+
+
+#### To-Do
+- [ ] Schedule asset materializations to occur once every month
---
+++ src/nhtsa_assets.py
@@ -0,0 +1,214 @@
+from datetime import datetime
+from dagster_snowflake_pandas import snowflake_pandas_io_manager
+from dagster import Definitions, SourceAsset, asset
+from tqdm import tqdm
+from utilities import fetch_manufacturers, fetch_model_names, fetch_wmi_by_manufacturer, fetch_wmi_data
+import io
+import pandas as pd
+import requests
+
+
+# The following ensures the tables listed below in Snowflake are available to be ingested by our assets
+manufacturers = SourceAsset(key='manufacturers')
+make_id_cars_trucks = SourceAsset(key='make_id_cars_trucks')
+make_id_cars_trucks.description = 'Table containing make IDs for cars and trucks only'
+wmi_by_manufacturer_id = SourceAsset(key='wmi_by_manufacturer_id')
+wmi_by_manufacturer_id.description = 'Table containing WMI codes by manufacturer ID'
+
+
+@asset
+def manufacturers(context) -> pd.DataFrame:
+    """
+    Vehicle manufacturer information from NHTSA API
+    """
+
+    df_list = []
+    page = 1
+    while True:
+        context.log.info(f"Fetching page {page}")
+        json_dict = fetch_manufacturers(page)     # imported from utilities
+        if json_dict['Count'] == 0:
+            context.log.info("Count is equal to zero/0 - exiting loop")
+            break
+        else:
+            # json_normalize() will drop records where the record_path contains an empty list
+            # To prevent this, see this SO question:
+            # https://stackoverflow.com/questions/63813378/how-to-json-normalize-a-column-in-pandas-with-empty-lists-without-losing-record
+            # For loop below is checking for "emptiness" of VehicleTypes, if empty,then fill with dictionary
+            for i, record in enumerate(json_dict['Results']):
+                if not record['VehicleTypes']:
+                    json_dict['Results'][i]['VehicleTypes'] = [{'IsPrimary': 'Null', 'Name': 'Null'}]
+
+            context.log.info(f"    Count={json_dict['Count']}")
+
+            df = pd.json_normalize(
+                json_dict['Results'],
+                record_path=['VehicleTypes'],
+                meta=['Country', 'Mfr_CommonName', 'Mfr_ID', 'Mfr_Name'],
+            )
+            df_list.append(df)
+            page = page + 1
+
+    df_combined = pd.concat(df_list, ignore_index=True)
+
+    today = datetime.today().strftime('%Y-%m-%d')
+    df_combined = df_combined.assign(Created_Date=today)
+    context.log.info(f"Number of rows in manufacturers dataframe: {df_combined.shape[0]}")
+
+    return df_combined[['Mfr_ID', 'Mfr_Name', 'Mfr_CommonName', 'Country', 'Created_Date']].drop_duplicates()
+
+
+@asset
+def manufacturer_id(manufacturers: pd.DataFrame) -> pd.DataFrame:
+    """
+    List of manufacturer IDs that we want to provide to downstream assets.
+
+    Parameters
+    ----------
+    manufacturers
+
+    Returns
+    -------
+    pandas dataframe with 'mfr_id' column
+
+    """
+
+    return manufacturers[['mfr_id', 'created_date']].drop_duplicates()
+
+
+@asset
+def makes() -> pd.DataFrame:
+    """
+    Vehicle makes from NHTSA API
+    """
+
+    df = pd.read_csv('https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=csv')
+    today = datetime.today().strftime('%Y-%m-%d')
+    df = df.assign(created_date=today)
+
+    return df
+
+
+@asset
+def model_names(make_id_cars_trucks: pd.DataFrame) -> pd.DataFrame:
+    """
+    Vehicle model names from NHTSA API (passenger cars and trucks only, last 15 years)
+
+    Parameters
+    ----------
+    make_id_cars_trucks: this is a smaller set of IDs since if we're to do all make IDs, then the downstream
+    process of obtaining model names would take a significant amount of time.
+
+    Returns
+    -------
+    A pandas dataframe containing model name information
+    """
+
+    # A good source for vehicle makes: https://cars.usnews.com/cars-trucks/car-brands-available-in-america
+
+    # Instead of hard-coding that we want last 15 model years, we can programmatically define the years for us
+    current_year = datetime.today().year
+    start_year = datetime.today().year - 14
+
+    df_list = []
+    progress_bar = tqdm(total=(current_year - start_year + 1) * len(make_id_cars_trucks) * 2)  # initialize progress bar
+
+    for year in range(start_year, current_year + 1):
+        for make_id in make_id_cars_trucks['make_id']:
+            for vehicle_type in ['passenger', 'truck']:
+                try:
+                    response = fetch_model_names(make_id=make_id, model_year=year, vehicle_type=vehicle_type)
+                    response.raise_for_status()
+                except requests.exceptions.HTTPError as e:
+                    print(f"HTTP error occurred: {e}")
+                    continue
+                csv_file = io.StringIO(response.content.decode('utf-8'))
+                df = pd.read_csv(csv_file)
+                df = df.assign(year=year)
+                # Some model_names can "look" like int types and so we want to make sure they are explicitly defined as str
+                # when concatenating the dataframes together.  Otherwise, will get a pyarrow error due to int/str confusion.
+                # Relevant background: https://github.com/wesm/feather/issues/349
+                df['model_name'] = df['model_name'].astype('str')
+                df_list.append(df)
+                progress_bar.update(1)  # increment progress bar
+
+    df_concat = pd.concat(df_list, ignore_index=True)
+    today = datetime.today().strftime('%Y-%m-%d')
+    df_concat = df_concat.assign(Created_Date=today)
+
+    return df_concat.drop_duplicates()
+
+
+@asset
+def wmi_by_manufacturer_id(manufacturer_id: pd.DataFrame) -> pd.DataFrame:
+    """
+    WMI codes by manufacturer using NHTSA's API.
+    It will take up to approximately 2 hours to materialize this asset.
+    """
+
+    df_list = []
+    for mfr_id in tqdm(manufacturer_id['mfr_id']):
+        df = fetch_wmi_by_manufacturer(mfr_id)      # imported from utilities
+        if df is not None:
+            df_list.append(df)
+
+    df_concat = pd.concat(df_list, ignore_index=True)
+    today = datetime.today().strftime('%Y-%m-%d')
+    df_concat = df_concat.assign(Created_Date=today)
+
+    return df_concat.drop_duplicates()
+
+
+@asset
+def wmi_with_makes(wmi_by_manufacturer_id: pd.DataFrame) -> pd.DataFrame:
+    """
+    WMI codes with vehicle make information from NHTSA's API.
+
+    Parameters
+    ----------
+    wmi_by_manufacturer_id
+
+    Returns
+    -------
+    pandas dataframe
+    """
+
+    df_list = []
+    for wmi in tqdm(wmi_by_manufacturer_id['wmi']):
+        df = fetch_wmi_data(wmi)          # imported from utilities
+        if df is not None:
+            df_list.append(df)
+
+    df_concat = pd.concat(df_list, ignore_index=True)
+    today = datetime.today().strftime('%Y-%m-%d')
+    df_concat = df_concat.assign(Created_Date=today)
+
+    return df_concat.drop_duplicates()
+
+
+defs = Definitions(
+    assets=[
+        manufacturers,
+        manufacturer_id,
+        makes,
+        make_id_cars_trucks,
+        model_names,
+        wmi_by_manufacturer_id,
+        wmi_with_makes,
+    ],
+    resources={
+        "io_manager": snowflake_pandas_io_manager.configured(
+            {
+                "account": {"env": "SF_ACCOUNT"},
+                "warehouse": {"env": "SF_WAREHOUSE"},
+                "database": {"env": "SF_DATABASE"},
+                "schema": "nhtsa",
+                "role": {"env": "SF_ROLE"},
+                "user": {"env": "SF_USERNAME"},
+                "password": {"env": "SF_PASSWORD"},
+                "authenticator": {"env": "SF_AUTHENTICATOR"},
+            }
+        )
+    },
+)
+
